a94ac001673e0b5d93852d2841d9c39c467208d2
goropikari <goropikari56@gmail.com>
2025-02-06 20:32:20 +0900

    comment dev config

--- .devcontainer/nvim/init.lua
+++ .devcontainer/nvim/init.lua
@@ -72,12 +72,12 @@ require("lazy").setup({
       end,
     },
   },
-  dev = {
-    ---@type string | fun(plugin: LazyPlugin): string directory where you store your local plugin projects
-    path = "/workspaces",
-    ---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
-    patterns = { "willothy" }, -- For example {"folke"}
-    fallback = true, -- Fallback to git when local plugin doesn't exist
-  },
+  -- dev = {
+  --   ---@type string | fun(plugin: LazyPlugin): string directory where you store your local plugin projects
+  --   path = "/workspaces",
+  --   ---@type string[] plugins that match these patterns will use your local versions instead of being fetched from GitHub
+  --   -- patterns = { "willothy" }, -- For example {"folke"}
+  --   fallback = true, -- Fallback to git when local plugin doesn't exist
+  -- },
   checker = { enabled = true },
 })
